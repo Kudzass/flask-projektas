5c1bbbe3c68669fef82e239cd94a4621c1e2ffb6
Name Surname“Tomas Sipaitisgit config --global user.name Tomas <t.sipaitis@gmail.com>
2025-04-08 19:56:34 +0300

    Testuojam SECRET_KEY spausdinimą

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
+print("SECRET_KEY iš aplinkos:", app.config['SECRET_KEY'])
+
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
